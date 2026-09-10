70a0ebae915577e066098a010409818637e07fd5
lukas <lukas@LUKAS>
2026-09-10 20:28:54 +0200

    Second commit

--- Övning_Coffee_Machine/Program.cs
+++ Övning_Coffee_Machine/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int smallPrice = 10;
+            int mediumPrice = 20;
+            int largePrice = 30;
+
+            Console.WriteLine("Please select your coffee size:");
+            int userchoice = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Do you want to buy more coffee? Yes or No");
+            string buyMore = Console.ReadLine();
+
         }
     }
 }
