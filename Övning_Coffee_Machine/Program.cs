dca09686055ca434b77bdbf4bc5458e8b2f9c95e
lukas <lukas@LUKAS>
2026-09-10 20:22:17 +0200

    Add project files.

---
+++ Övning_Coffee_Machine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Övning_Coffee_Machine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
