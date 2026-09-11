96a56db4488f35ab2d0848c60a55da46926c46cb
lukas <lukas@LUKAS>
2026-09-11 15:50:31 +0200

    Final version

--- Övning_Coffee_Machine/Program.cs
+++ Övning_Coffee_Machine/Program.cs
@@ -7,13 +7,43 @@
             int smallPrice = 10;
             int mediumPrice = 20;
             int largePrice = 30;
+            int totalPrice = 0;
+            string buyMore = "";
 
-            Console.WriteLine("Please select your coffee size:");
-            int userchoice = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Please select your coffee size: 1 - Small 2 - Medium 3 - Large");
+                if (!int.TryParse(Console.ReadLine(), out int userChoice))
+                {
+                    Console.WriteLine("Invalid input, please enter a number");
+                    continue;
+                }
+                    
+                
 
-            Console.WriteLine("Do you want to buy more coffee? Yes or No");
-            string buyMore = Console.ReadLine();
+                if (userChoice == 1)
+                {
+                    totalPrice = totalPrice + smallPrice;
+                }
+                else if (userChoice == 2)
+                {
+                    totalPrice = totalPrice + mediumPrice;
+                }
+                else if (userChoice == 3)
+                {
+                    totalPrice = totalPrice + largePrice;
+                }
+                else
+                {
+                    Console.WriteLine($"{userChoice} is not a valid option");
+                    continue;
+                }
 
+                Console.WriteLine("Do you want to buy more coffee? Yes or No");
+                buyMore = Console.ReadLine();
+            }
+            while (!buyMore.Equals("No", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine($"Total to pay: {totalPrice}");
         }
     }
 }
